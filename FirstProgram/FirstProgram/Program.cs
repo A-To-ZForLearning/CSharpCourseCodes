86ce26a402db607ed786457aeb0403c57f502674
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-09 22:23:18 -0400

    First C SHarp Program
    This is my first C Sharp program

---
+++ FirstProgram/FirstProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is my first Program in C#
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
