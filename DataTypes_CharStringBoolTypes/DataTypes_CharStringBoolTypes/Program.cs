0ed9001ba66548b5cb49b4603ae47ddbfb80b1e0
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-10 19:47:06 -0400

    DataTypes-CharStringBooleanTypes
    This is a program to explain these types in C3

---
+++ DataTypes_CharStringBoolTypes/DataTypes_CharStringBoolTypes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes_CharStringBoolTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int charMin = Char.MinValue;
+            int charMax = Char.MaxValue;
+
+            char myVar = 'G';
+            int myVarValue = 'G';
+
+            char myVarValue2 = Convert.ToChar(75);
+
+            Console.WriteLine("Min Char = {0} Max Char = {1}  My Character = {2} my Variable Number = {3} my Conversion value is = {4}",
+                charMin, charMax, myVar, myVarValue, myVarValue2);
+
+
+            string myString = "This is my variable";
+            Console.WriteLine("\nMy String is {0}", myString);
+
+            bool myBool = false;
+            Console.WriteLine("my boolean value is {0}", myBool);
+
+            Console.ReadKey();
+        }
+    }
+}
