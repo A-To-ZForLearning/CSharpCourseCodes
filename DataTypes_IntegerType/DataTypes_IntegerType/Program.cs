2ae85d116b1814b159d0fbda0a44dd06a28d0590
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-10 13:17:07 -0400

    DataTypes-IntegerType

---
+++ DataTypes_IntegerType/DataTypes_IntegerType/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes_IntegerType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte sbyteMinVal = SByte.MinValue;
+            sbyte sbyteMaxVal = SByte.MaxValue;
+            Console.WriteLine("Min Value = {0}  Max Value = {1}", sbyteMinVal, sbyteMaxVal);
+
+
+            Console.ReadKey();
+
+
+            //Q1/ Define an integer variable and assign a value and print that on console screen
+            //Q2/ Define another variable and assign another value and sum both numbers and save it in another variable and print the result
+            //Q3/ print the Min Value and the Max value of the types that we didn't mention their values?
+            /* 
+                     Q4/ What's the error message that you got and in which one of the options below?
+                         long longNum = 1000;
+                         int intNum = 1000;
+                         1 - int temp1 = longNum + intNum;
+                         2 - long temp2 = longNum + intNum;
+
+                        And Why?
+
+                */
+
+            /* for more information go to A-To-Z For LEarning channel in this link https://goo.gl/7SCOj3 */
+
+
+
+        }
+    }
+}
