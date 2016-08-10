9d50b8bf162f261aa0da593d10036dd41d1de9e5
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-10 00:12:10 -0400

    DataTypes-IntegerType
    This program about the integer data type

---
+++ DataTypes_IntegerType/DataTypes_IntegerType/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes_IntegerType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte sbyteMinVal = SByte.MinValue;
+            sbyte sbyteMaxVal = SByte.MaxValue;
+            Console.WriteLine("Min Value = {0}  Max Value = {1}", sbyteMinVal, sbyteMaxVal);
+
+            Console.ReadKey();
+
+            //Q1/ Define an integer variable and print that on console screen
+            //Q2/ Define another variable and sum both numbers and save it in another variable and print the result
+            /* 
+                     Q3/ Which one is right ?
+                         double doubleNum = 1000;
+                         int intNum = 1000;
+                         1 - int temp1 = doubleNum + intNum;
+                         2 - double temp2 = doubleNum + intNum;
+
+                        And Why?
+
+                */
+				
+				/*
+				For more information go to the A-To-Z ForLearning channel
+				Link: https://goo.gl/gDuvjs
+				
+				*/
+
+
+        }
+    }
+    }
+}
