47cd346130116726273e2d1ee2d4d84f8876685b
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-11 19:30:41 -0400

    Increments-Decrements Operations
    In this lesson we will discuss the increments and decrements operations
    and how could we apply both of them

---
+++ Increments-Decrements/Increments-Decrements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Increments_Decrements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int myVar1 = 2;
+            int temp1 = myVar1--;
+            Console.WriteLine("{0}", temp1);
+
+            Console.ReadKey();
+
+
+            /*
+                        Q1 / WHat's the result of temp1 = var1++ if the value of var1 = 5;
+                        Q2 / WHat's the result of temp1 = ++var1 if the value of var1 = 5;
+
+                        Q3 / What's the result of this expression
+                          int temp, myVar1 = 7, myVar2 = 2;
+
+                        temp = myVar1-- / ++myVar2;
+
+                        and Why;
+                        */
+            /* for more information go to A-To-Z For LEarning channel in this link https://goo.gl/7SCOj3 */
+        }
+    }
+}
