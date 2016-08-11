8c4f5dfed4c8f6064833c68bf2884e83d897b141
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-11 13:08:10 -0400

    Literal Character
    In this lesson we will discuss the literal values that we must put them
    after some data types in the C Sharp like float and decimal

---
+++ Literal_Characters/Literal_Characters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Literal_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar = -100;
+            uint myUVar = 1000U;
+        
+            float myFlVar = 1.5F;
+            double myDVar = 100.5;
+            decimal myDecVar = 100.5m;
+
+            Console.WriteLine("{0} {1}", myVar, myUVar);
+            Console.WriteLine($"{myDecVar}, {myDVar}");
+            Console.ReadKey();
+
+            /* for more information go to A-To-Z For LEarning channel in this link https://goo.gl/7SCOj3 */
+        }
+    }
+}
