d7153a8065c1226f13fe8af892efe71915627b96
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-11 19:32:21 -0400

    Assignments operations
    In this lesson we will discuss the different assignemnts operations and
    know what's the  difference among them...we have a challenge here, put
    your solution under the video.

---
+++ Assignments Operations/Assignments Operations/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //=, +=, -=, *=, /=, %=, 
+
+            int myVar1 = 7, myVar2 = 3;
+
+            //myVar2 = myVar1;
+
+            // myVar2 += myVar1;    //myVar1 = myVar1 + myVar2
+
+         //   myVar1 -= myVar2;      //myVar1 = myVar1 - myVar2
+
+            myVar1 %= myVar2;     //myVar1 = myVar1 % myVar2
+
+            Console.WriteLine("The Result is = {0} The values of Var2 =  {1}", myVar1,myVar2);
+            Console.ReadKey();
+            /*
+
+            Q1/ What's the result of this expression
+               int  temp, myVar1 = 7, myVar2 = 2;
+
+              myVar1 /= ++myVar2; 
+                     and Why;
+
+           */
+
+            /* for more information go to A-To-Z For LEarning channel in this link https://goo.gl/7SCOj3 */
+
+
+        }
+    }
+}
