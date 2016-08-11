0bec464e649a636937a0f32ca2884de03e9a4356
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-11 19:28:21 -0400

    Expressions
    In this lesson we will learn about the expression in the C Sharp

---
+++ Expressions/Expressions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float myVar = 10.5f;
+            float myVar2 = 10.7f;
+
+            float temp = myVar + myVar2;
+
+            //basic operators  + - / * 
+            //bitwise operations 
+            //logical operators Boolean true , false
+
+
+            //1- unary operand
+            //2- Binary operands
+            //3- 3 operands 
+
+
+            /* for more information go to A-To-Z For LEarning channel in this link https://goo.gl/7SCOj3 */
+
+
+
+
+        }
+    }
+}
