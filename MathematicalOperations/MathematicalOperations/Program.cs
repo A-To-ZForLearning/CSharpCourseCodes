e9c8799b1634f4f5ca3102603fa267bcd18cf6c4
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-11 19:29:19 -0400

    Mathematical Operations
    In this lesson we will discuss the expression in the C Sharp

---
+++ MathematicalOperations/MathematicalOperations/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicalOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // + - / * %
+
+            int myvar1 = 15;
+            int myVar2 = 10;
+            int temp = myvar1 + myVar2;
+            int temp2 = myvar1 - myVar2;
+            int temp3 = myvar1 % myVar2;
+
+
+            // + -
+            int myNegtiveVar = -10;
+
+            int temp4 = + myvar1;
+            int temp5 = -myvar1;
+            int temp6 = +myNegtiveVar;
+            Console.WriteLine("{0}\n\n{1}\n\n{2}", temp4, temp5, temp6);
+            //Console.WriteLine("The result is {0}  The result2 is {1} The remaining is {2}", temp, temp2, temp3);
+            Console.ReadKey();
+
+            /* for more information go to A-To-Z For LEarning channel in this link https://goo.gl/7SCOj3 */
+        }
+    }
+}
