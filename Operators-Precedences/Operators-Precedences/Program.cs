fe2d73f6f7d1148e3b12a91564926ea40d3c4ac2
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-12 08:35:08 -0400

    Operatoes Precedences
    In this lesson we covered the operators precedences

---
+++ Operators-Precedences/Operators-Precedences/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Precedences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // + - * / %  ++ -- = += -= *= /= %= ++ --
+
+            int myVar1 = 6, myVar2 = 7, myVar3 = 3, temp, temp2;
+
+           // temp = myVar1 + myVar2;
+
+            //temp = myVar1 + myVar2 * myVar3;
+            //temp = myVar1 * myVar2 / myVar3;
+             temp = (myVar1 + ++myVar2) * --myVar3;
+
+            myVar1 = 6;
+            myVar2 = 7;
+            myVar3 = 3;
+            
+            temp2 = (myVar1 + myVar2++) * myVar3--;
+
+            Console.WriteLine("{0}\n\n{1}",temp,temp2);
+            Console.ReadKey();
+   
+
+            /*
+Highest Operators   ++, -- (used as prefixes); +, - (unary)
+                    *, /, %
+                    +, -
+                    =, *=, /=, %=, +=, -=
+Lowest  Operators   ++, -- (used as postfixes)
+*/
+
+
+           /*
+                        Q1 / try to get the result 15 of these variables
+
+                         myVar1 = 1
+                         myVar2 = 3
+                         myVar3 = 4
+                         myVar4 = 2
+                         myVar5 = 11
+
+
+                         ++myVar1 + myVar2 * --myVar3 / ++myVar4 + --myVar5;
+
+                */
+
+
+
+
+
+
+ /* for more information go to A-To-Z For LEarning channel in this link https://goo.gl/7SCOj3 */
+
+
+
+
+
+
+
+        }
+    }
+}
