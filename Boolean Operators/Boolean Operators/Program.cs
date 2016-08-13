dd1081d07b20f685deb7c0052033e7ffc7eba61e
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-13 09:37:28 -0400

    Boolean Operators
    In this lesson we discussed the boolean Operators both
    1- Comparisons
    2- Conditions
    With a challenge

---
+++ Boolean Operators/Boolean Operators/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Boolean Comparison   ==, !=, <, >, <=, >=
+            // Boolean Conditions  &&, ||
+
+            bool myResult;
+            int myVar1 = 5, myVar2 = 8;                                               //  &  Table Truth
+            myResult = myVar1 != 8;                                                   //  F  F --> F
+                                                                                      //  T  F --> F 
+            myResult = (myVar1 <= 5) && (myVar2 > 8);                                //  F  T --> F 
+                                                                                      //  T  T --> T
+            Console.WriteLine("MyResult is ={0}", myResult);
+            Console.ReadKey();
+
+            /*
+  Q1 /Define myVar1 = 9, myVAr2=11 and  Print the result of this conditions
+  1- (myVar1 <10) && (myVar2<=11)
+  2- (myVar1 <10) || (myVar2<=11)
+  and Why?
+     */
+
+            /* for more information go to A-To-Z For LEarning channel in this link https://goo.gl/7SCOj3 */
+        }
+    }
+}
