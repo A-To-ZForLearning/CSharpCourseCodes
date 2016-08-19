5dd3d548345f5931e8ab5542a27d82cdf985ab40
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-19 16:19:45 -0400

    Challenge2
    This is the solution of the challenge 2

---
+++ Challenges Solutions/Challenge2/Challenge2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int myVar1 = 'A', myVar2= 'a';
+            string myString1 = "This is my character A value ", myString2 = " This is my character a value ";
+            Console.WriteLine("\"" + myString1 + myVar1 + myString2 + myVar2 + "\"");
+            Console.ReadKey();
+
+
+
+
+
+          /*  for more information visit
+              http://www.youtube.com/c/AToZForLearning 
+           */
+        }
+    }
+}
