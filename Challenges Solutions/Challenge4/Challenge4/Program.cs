aaa96ac5b221e189d06730ece69ca89c3d34eff6
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-19 16:35:51 -0400

    Challenge4
    This is a solution of challenge4

---
+++ Challenges Solutions/Challenge4/Challenge4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar1 = 7, myVar2 = 3, temp;
+
+            temp = myVar1 + myVar2;
+            Console.WriteLine("myVar1 + myVar2 = " + "{0}", temp);
+
+            temp = myVar1 - myVar2;
+            Console.WriteLine("myVar1 - myVar2 = " + "{0}", temp);
+
+            temp = myVar1 * myVar2;
+            Console.WriteLine("myVar1 * myVar2 = " + "{0}", temp);
+
+            temp = myVar1 / myVar2;
+            Console.WriteLine("myVar1 / myVar2 = " + "{0}", temp);
+
+            temp = myVar1 % myVar2;
+            Console.WriteLine("myVar1 % myVar2 = " + "{0}", temp + "\n");
+
+
+            string myStr1 = "I am", myStr2 = " smart";
+            Console.WriteLine("{0}", myStr1 + myStr2);
+
+
+            Console.ReadKey();
+             
+
+
+            /*  for more information visit
+          http://www.youtube.com/c/AToZForLearning 
+       */
+        }
+    }
+}
