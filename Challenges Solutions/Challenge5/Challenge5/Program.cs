967934050d4209d04ec7266fec6c98e558e72b80
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-19 16:47:17 -0400

    Challenge5
    This a solution of the challenge5

---
+++ Challenges Solutions/Challenge5/Challenge5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp1, temp2, myVar1 = 5, temp3;
+            temp1 = myVar1++;
+
+            myVar1 = 5;
+            temp2 = ++myVar1;
+            Console.WriteLine("The value of myVar++ is {0},"+
+                " While the result of ++myVar is {1}", temp1, temp2);
+
+                myVar1 = 7;
+            int myVar2 = 2;
+
+            temp3 = myVar1-- / ++myVar2;
+            Console.WriteLine("The result of this expression (myVar1-- / ++myVar2) is {0}", temp3);
+
+            //I still looking for the answer Why?
+            Console.ReadKey();
+
+
+
+            /*  for more information visit
+               http://www.youtube.com/c/AToZForLearning 
+            */
+        }
+    }
+}
