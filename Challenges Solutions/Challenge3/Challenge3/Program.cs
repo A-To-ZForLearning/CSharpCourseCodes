b6d560f0ab0e0e05b7118bbdbf60f7d2ef109ac7
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-19 16:26:28 -0400

    Challenge3
    This is a solution of the challenge3

---
+++ Challenges Solutions/Challenge3/Challenge3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myStr = @"Nazar has said ""This is args unique course,"" and I think so ";
+
+            Console.WriteLine(myStr);
+            Console.ReadKey();
+            //Q2/ You can search about the purpose of it online in MSDN
+
+            /*  for more information visit
+             http://www.youtube.com/c/AToZForLearning 
+          */
+        }
+    }
+}
