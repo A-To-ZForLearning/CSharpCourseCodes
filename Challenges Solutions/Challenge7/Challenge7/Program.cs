e36b29282fdf789007006d6e7242b840759f83a6
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-19 17:05:11 -0400

    Challenge7
    This is a solution of the challenge7

---
+++ Challenges Solutions/Challenge7/Challenge7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar1 =9, myVar2 =11  ;
+            bool myConditionResult;
+
+            myConditionResult = ((myVar1 <= 9) && (myVar2 <= 11));
+            Console.WriteLine("The Result of my condition is {0}", myConditionResult);
+            myConditionResult = ((myVar1 <= 9) || (myVar2 <= 11));
+            Console.WriteLine("The Result of my condition is {0}", myConditionResult);
+
+            Console.ReadKey();
+
+
+            /*  for more information visit
+               http://www.youtube.com/c/AToZForLearning 
+            */
+
+
+        }
+    }
+}
