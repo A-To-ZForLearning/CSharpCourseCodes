ebf840a70cdc61c72c55e20dbb8080260a9fbe0d
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-20 09:54:07 -0400

    Challenge10
    This is a solution of the Challenge10

---
+++ Challenges Solutions/Challenge10/Challenge10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string whatHeSay = "Hello";
+
+            switch (whatHeSay)
+            {
+                case "hello":
+                    Console.WriteLine("My animal is the canary bird");
+                    break;
+                case "Bye":
+                    Console.WriteLine("My animal is a cat");
+                    break;
+                case "welcome":
+                    Console.WriteLine("My animal is a camel");
+                    break;
+
+                default:
+                    Console.WriteLine("no one");
+                    break;
+
+            }
+            /*  for more information visit
+        http://www.youtube.com/c/AToZForLearning 
+     */
+            Console.ReadKey();
+        }
+    }
+}
