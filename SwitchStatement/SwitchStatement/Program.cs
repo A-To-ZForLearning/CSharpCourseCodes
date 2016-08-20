b967090d23349c0c774bfad359c02f7f96a7c4a6
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-20 09:50:52 -0400

    Switch Statement
    In this lesson we discussed the switch statement and its cases

---
+++ SwitchStatement/SwitchStatement/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string myAnimal = "Cat";
+
+            //switch (myAnimal)
+            //{
+            //    case "Canary":
+            //        Console.WriteLine("My animal is the canary bird");
+            //        break;
+            //    case "Cat":
+            //        Console.WriteLine("My animal is a cat");
+            //        break;
+            //    case "Camel":
+            //        Console.WriteLine("My animal is a camel");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("no one");
+            //        break;
+
+            //}
+
+            int myVar = 15;
+            switch (myVar)
+            {
+                case 10:
+                    Console.WriteLine("My Variable number value is 10");
+                    break;
+                case 7:
+                    Console.WriteLine("My Variable number value is 7");
+                    break;
+                case 15:
+                    Console.WriteLine("My Variable number value is 15");
+                    break;
+
+                default:
+                    Console.WriteLine("no one");
+                    break;
+
+            }
+
+            //Challenge 10
+            //Define a string variable whatHesay and assign the value "Hello" and check what the variable 
+            //value among these cases  "hello", "Bye", "welcome" or NoOne is the defult and print the rsult 
+            // and Why?
+
+            Console.ReadKey();
+
+            /*  for more information visit
+        http://www.youtube.com/c/AToZForLearning 
+     */
+
+        }
+    }
+}
