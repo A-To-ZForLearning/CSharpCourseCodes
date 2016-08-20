bd138efe82b295fd70d6a32d0e66fb9ac8083ff5
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-20 08:55:51 -0400

    Nested If Else2
    In this lesson we have covered more about using conditions

---
+++ NestedIfElse/NestedIfElse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedIfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int myVar = 4, myVar2 = 6;
+            if (myVar <= 3 && myVar2 >= 5)
+            {
+                Console.WriteLine("Condition one right");
+            }
+            else if (myVar <= 4 && myVar2 >= 6)
+            {
+                Console.WriteLine("Condition two right");
+            }
+            else if (myVar2 > 5)
+            {
+                Console.WriteLine("Condition three right");
+            }
+            else
+            {
+                Console.WriteLine("No one of the conditions");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
