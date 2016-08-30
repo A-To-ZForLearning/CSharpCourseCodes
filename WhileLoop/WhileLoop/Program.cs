07c3efcd2665d49a7bd8acfcdb6472f077edcd1e
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-30 18:26:38 -0400

    While Loop
    In this lesson we discussed the while ieration loop and the challenge 12

---
+++ WhileLoop/WhileLoop/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            double mySalary = 1020.30, total = 0.0;
+
+            while (i < 15)
+            {
+                total += mySalary;
+                i++;
+            }
+            Console.WriteLine("My Salary is {0}", total);
+            Console.ReadKey();
+
+        }
+
+
+                //Q1 Find how many exercises do need to reach 83 kg if your weight was 120kg and
+                //each exercise will make you lose 1kg and print the result;
+
+ /*  for more information visit
+          http://www.youtube.com/c/AToZForLearning 
+ */
+        }
+    
+}
