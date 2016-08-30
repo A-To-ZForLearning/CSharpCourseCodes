5a269a0abbb58e9f0558cb705cfc42df071ea14b
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-30 18:51:21 -0400

    Braek_Loop-ContinueCommand
    In this lesson we discussed the continue command and how it interrupt
    the loops and the challenge 16

---
+++ Braek_Loop-ContinueCommand/Braek_Loop-ContinueCommand/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break_Loop_BreakCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Hello Nazar");
+                if (i > 2)
+                    continue;
+
+                for (int j = 0; j <= 4; j++)
+                {
+                    Console.WriteLine("\t Hello World");
+                }
+
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+
+
+
+            //Q16/ Same Q/15 using continue command
+
+
+            /*  for more information visit
+                     http://www.youtube.com/c/AToZForLearning 
+            */
+        }
+    }
+}
