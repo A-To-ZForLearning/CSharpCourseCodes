80792c2eef9d7c8a61f275e521ae86c111003c47
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-30 18:48:53 -0400

    Break_Loop_BreakCommand
    In this lesson we have discussed the break command and how it works and
    the challenge 15

---
+++ Break_Loop_BreakCommand/Break_Loop_BreakCommand/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break_Loop_BreakCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Hello Nazar");
+                if (i > 2)
+                    break;
+
+                for (int j = 0; j <= 4; j++)
+                {
+                    Console.WriteLine("\t Hello World");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+
+
+
+            //Q15/ Create Multiplication table for number in the range 1-10
+            // and stop the loop in the multiplication table for the number 7 from 1-10;
+
+
+        }
+    }
+}
