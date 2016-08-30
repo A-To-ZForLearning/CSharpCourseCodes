e8f25062b95edfd5224794e26712f44b9a28c58c
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-08-30 19:42:11 -0400

    For Loop
    In this lesson we discussed the for loop and give the challenge 13

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mySalary = 1000, total = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                total += mySalary;
+                Console.WriteLine("My Salary for 12 months is {0}", total);
+            }
+           
+            Console.ReadKey();
+
+
+
+            //Challenge 13 
+            //Convert the questions in the Do-While and the While to for loop
+
+            /*  for more information visit
+                     http://www.youtube.com/c/AToZForLearning 
+            */
+        }
+    }
+}
