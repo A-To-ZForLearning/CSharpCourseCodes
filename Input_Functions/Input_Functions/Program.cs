efcf3b53c30acca3a1d217d973bb64f56fcaa569
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-09-02 03:31:09 -0400

    .
    .

---
+++ Input_Functions/Input_Functions/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Input_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ReadLine  , Read
+            string myName;
+            int myVar1, myVar2;
+            Console.WriteLine("Please Enter Your name \n");
+            myName = Console.ReadLine();
+
+            Console.WriteLine("\nPlease Enter Your first Number \n");
+
+            myVar1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Your Entry ");
+            myVar2 = Console.Read();
+            Console.WriteLine($"Hello {myName}");
+            Console.WriteLine($"You Entered The Number {myVar1}");
+            Console.WriteLine($"You Entered The Entery {myVar2}");
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+            /*
+             * //Q19/ Define 4 variables 
+             * 1- First Name
+             * 2- Last Name
+             * 3- Salary
+             * 4- TaxRate =  0.07
+             * 5- Enter all these values from the console window and then
+             * Find the salary for 12 month and cut the TaxRate for 12 months and print the salary
+             * in this form
+             * Welcome fName, LName
+             * Your Salary for 12 months is #$
+             * You paid TaxRate for 12 months  #$
+             * Good Bye
+             * */
+
+            for more information visit
+         http://www.youtube.com/c/AToZForLearning 
+
+Visit our channel page on facebook
+         https://www.facebook.com/AToZForLearning/
+
+If you have any question in English or Arabic welcome to our facebook group
+         https://www.facebook.com/groups/AToZForLearningProgramming/  
+
+
+         */
+
+        }
+    }
+}
