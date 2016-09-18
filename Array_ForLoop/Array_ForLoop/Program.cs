125a104c46c4bcb029ad8a20b29304cb13b3e7c5
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-09-18 01:18:05 -0400

    Array and challenge 22
    Arraya Discussion and challenge 22

---
+++ Array_ForLoop/Array_ForLoop/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          const  int myArraySize = 5;
+            int[] myNumbers = { 1, 5, 8, 9, 7 };
+            int[] myNumbers2 = new int[myArraySize];
+            int[] myNumbers3 = new int[myArraySize] {1,5,8,9,7 };
+
+            for(int i=0; i<5;i++)
+            Console.WriteLine("My numbers " + (i + 1) + "= " + $"{myNumbers[i]}" + " ");
+            Console.ReadKey();
+
+
+            //Q22/ Define an array of int type and find the max value 
+
+
+
+
+            /*  
+
+for more information visit
+         http://www.youtube.com/c/AToZForLearning 
+
+Visit our channel page on facebook
+         https://www.facebook.com/AToZForLearning/
+
+If you have any question in English or Arabic welcome to our facebook group 
+         https://www.facebook.com/groups/AToZForLearningProgramming/  
+
+
+         */
+         
+        }
+    }
+}
---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        enum humanSex :sbyte
+            {
+            male =  1,
+            female = 2
+
+           }
+        static void Main(string[] args)
+        {
+            sbyte humanSbyte;
+           
+            humanSex myhumanSex = humanSex.male;
+            Console.WriteLine($"Human Sex =  {myhumanSex}" + "\n" );
+            humanSbyte = (sbyte)myhumanSex;
+            Console.WriteLine($"Human Gender index in  = {humanSbyte}" + "\n");
+            string humanGender;
+            humanGender = Convert.ToString(myhumanSex);
+            Console.WriteLine($"Human associated with this index = {humanGender}" + "\n");
+            Console.ReadKey();
+
+
+
+
+            //Q20/ Define an Enum variable for orientation and print the contents usign loop
+            //and start from index 2
+
+
+
+
+            /*  
+
+for more information visit
+         http://www.youtube.com/c/AToZForLearning 
+
+Visit our channel page on facebook
+         https://www.facebook.com/AToZForLearning/
+
+If you have any question in English or Arabic welcome to our facebook group 
+         https://www.facebook.com/groups/AToZForLearningProgramming/  
+
+
+         */
+        }
+    }
+}
