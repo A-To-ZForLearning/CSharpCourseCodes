f8b3f2df11488401661c08078569ac5258e72451
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-09-18 19:12:39 -0400

    ArrayForeachand Challenge 23

---
+++ Array+Foreach/Array+Foreach/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int myArraySize = 5;
+            int[] myNumbers3 = new int[myArraySize] { 1, 5, 8, 9, 7 };
+
+               // for (int i = 0; i <= 5 ; i++)
+                //Console.WriteLine("My numbers " + (i + 1) + "= " + $"{myNumbers3[i]}" + " ");
+
+            foreach (int item in myNumbers3)
+            {
+                Console.WriteLine("My numbers is " + $"{item}");
+
+            }
+            Console.ReadKey();
+
+
+            //Q23/ Define an array of 15 elements and use foreach to iterate 14 elements only
+            /*  
+
+for more information visit
+         http://www.youtube.com/c/AToZForLearning 
+
+Visit our channel page on facebook
+         https://www.facebook.com/AToZForLearning/
+
+If you have any question in English or Arabic welcome to our facebook group 
+         https://www.facebook.com/groups/AToZForLearningProgramming/  
+
+
+         */
+
+        }
+    }
+}
