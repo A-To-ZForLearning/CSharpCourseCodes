15878dac5904e1b03a9da64d3bf2d488d9ef24e2
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-09-19 18:26:34 -0400

    String Manipulation Operations

---
+++ StringManipulationOperations/StringManipulationOperations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulationOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string myString = "Hello My Friends";
+            char[] myCharArray = myString.ToCharArray();
+
+            Console.WriteLine($"The String Length is {myString.Length}\n");
+
+            foreach (char mychar in myCharArray)
+            {
+
+                Console.WriteLine($"{mychar}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
