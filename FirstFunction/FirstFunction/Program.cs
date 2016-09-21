6251cdc80191ec957f18e077f22ca6fcf79d9c03
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-09-21 07:36:38 -0400

    FirstFunction

---
+++ FirstFunction/FirstFunction/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstFunction
+{
+    class Program
+    {
+
+        static void print_Name()
+        {
+            Console.Write("My Name Is Mohmmed");
+        }
+
+        static void Main(string[] args)
+        {
+            print_Name();
+            Console.ReadKey();
+
+
+            /*  
+
+for more information visit
+         http://www.youtube.com/c/AToZForLearning 
+
+Visit our channel page on facebook
+         https://www.facebook.com/AToZForLearning/
+
+If you have any question in English or Arabic welcome to our facebook group 
+         https://www.facebook.com/groups/AToZForLearningProgramming/  
+
+
+         */
+
+
+        }
+    }
+}
