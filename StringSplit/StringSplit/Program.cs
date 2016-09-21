f0a9368a5de5894c2fbc8087bea8f902bd05110f
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-09-20 21:08:19 -0400

    SplitFunction

---
+++ StringSplit/StringSplit/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString;
+            char[] separator = { ' ', 'u' };
+            myString = Console.ReadLine();
+            string[] myNewWords;
+            myNewWords = myString.Split(separator);
+            foreach (string word in myNewWords)
+            {
+                Console.WriteLine($"{word}");
+            }
+            Console.ReadKey();
+            /*  
+
+for more information visit
+         http://www.youtube.com/c/AToZForLearning 
+
+Visit our channel page on facebook
+         https://www.facebook.com/AToZForLearning/
+
+If you have any question in English or Arabic welcome to our facebook group 
+         https://www.facebook.com/groups/AToZForLearningProgramming/  
+
+
+         */
+
+        }
+    }
+}
