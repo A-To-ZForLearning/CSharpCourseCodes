b0389475083489decbba5f3f02ad828c507ba626
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-09-21 07:59:50 -0400

    ParametersVsArguments

---
+++ ParamertersVsArguments/ParamertersVsArguments/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamertersVsArguments
+{
+    class Program
+    {
+        static void addNumbers(int myFirstNumber, int mySecondNumber)
+        {
+            int temp = myFirstNumber + mySecondNumber;
+            Console.WriteLine($"{temp}");
+        }
+
+        static int addNumbers2(int myFirstNumber, int mySecondNumber)
+        {
+            int temp = myFirstNumber + mySecondNumber;
+            return temp;
+        }
+        static void Main(string[] args)
+        {
+            int myVar1 = 5, myVar2 = 3;
+
+            addNumbers(myVar1, myVar2);
+
+             
+            Console.WriteLine($"{addNumbers2(myVar1, myVar2)}");
+            Console.ReadKey();
+
+            /*  
+
+for more information visit
+         http://www.youtube.com/c/AToZForLearning 
+
+Visit our channel page on facebook
+         https://www.facebook.com/AToZForLearning/
+
+If you have any question in English or Arabic welcome to our facebook group 
+         https://www.facebook.com/groups/AToZForLearningProgramming/  
+
+
+         */
+        }
+    }
+}
---
+++ ParamertersVsArguments/ParamertersVsArguments/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ParamertersVsArguments")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ParamertersVsArguments")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d4c23b67-3b87-4a5b-82b4-c1c83b485971")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
