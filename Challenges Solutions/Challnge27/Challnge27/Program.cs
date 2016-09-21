af1ec370f6e2dc7f691ae2f19e828fa938863ef1
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-09-21 16:09:45 -0400

    Challenge 27

---
+++ Challenges Solutions/Challnge27/Challnge27/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challnge27
+{
+    class Program
+    {
+        static void DoubleNum(ref int val, ref int val2)
+        {
+            val *= 2;
+            val2 *= 4;
+            Console.WriteLine($"myNumber = {val}");
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            int myNumber = 5, myNumber2 = 4;
+            Console.WriteLine($"myNumber = {myNumber}");
+            DoubleNum(ref myNumber, ref myNumber2);
+            Console.WriteLine($"myNumber = {myNumber},{myNumber2}");
+            Console.ReadKey();
+
+            /*  
+
+            for more information visit
+                     http://www.youtube.com/c/AToZForLearning 
+
+           Visit our channel page on facebook
+                     https://www.facebook.com/AToZForLearning/
+
+           If you have any question in English or Arabic welcome to our facebook group 
+                     https://www.facebook.com/groups/AToZForLearningProgramming/  
+
+
+                     */
+        }
+    }
+}
