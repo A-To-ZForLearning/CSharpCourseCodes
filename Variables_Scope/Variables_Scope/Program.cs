4cc47adb67666cf392b4de4727b448f910207db2
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-09-21 23:19:52 -0400

    Variables Scope

---
+++ Variables_Scope/Variables_Scope/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables_Scope
+{
+    class Program
+    {
+        static string myString2;
+        static void printMyString()
+        {
+            string myString = "My String inside the printMyString Method ";
+            Console.WriteLine($"{myString}");
+            Console.WriteLine($"{ myString2}");
+
+        }
+
+        static void Main(string[] args)
+        {
+            string myString = "My String inside the Main Method";
+           myString2 = "Global myString variable";
+            printMyString();
+
+            Console.WriteLine($"{myString}");
+            Console.WriteLine($"{ myString2}");
+            Console.ReadKey();
+
+            /*  
+
+            for more information visit
+                     http://www.youtube.com/c/AToZForLearning 
+
+           Visit our channel page on facebook
+                     https://www.facebook.com/AToZForLearning/
+
+           If you have any question in English or Arabic welcome to our facebook group 
+                     https://www.facebook.com/groups/AToZForLearningProgramming/  
+
+
+                     */
+        }
+    }
+}
