f24dfef2b634336f6c108073d14207b07e9a33ec
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-09-29 15:42:56 -0400

    Delegate Type and Challenge 29

---
+++ DelegateType/DelegateType/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateType
+{
+    delegate int MyNewNumber(int myNumber);
+    class Program
+    {
+        static int myXNumber = 3;
+        static int addFunct(int number)
+        {
+            myXNumber += number;
+            return myXNumber;
+        }
+        static int mulFunc(int number)
+        {
+            myXNumber *= number;
+            return myXNumber;
+        }
+        static void Main(string[] args)
+        {
+            MyNewNumber n1 = new MyNewNumber(addFunct);
+            MyNewNumber n2 = new MyNewNumber(mulFunc);
+            n1(4);
+            Console.Write($"{myXNumber}\n");
+            n2(5);
+            Console.Write($"{myXNumber}");
+            Console.ReadKey();
+
+            //Q30
+            //Define a delegate to print the firstname in a printFirstNamefunction once
+            // and print the lastname in a printLastNamefunction
+
+        }
+    }
+}
