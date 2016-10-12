9b7d31390e2b8d15e55b32ef27fbb0b67091794f
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-10-12 01:02:38 -0400

    DLL+Namespacse

---
+++ OperationsOn_Namespaces/OldBrothers/MyBrothers.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+namespace Brothers
+{
+    namespace OldBrothers
+    {
+        public class MyBrothers
+        {
+            public static void print()
+            {
+                Write("\nMohmmed" + " and " + "Shamel");
+            }
+            /*  
+
+for more information visit
+         http://www.youtube.com/c/AToZForLearning 
+
+Visit our channel page on facebook
+         https://www.facebook.com/AToZForLearning/
+
+If you have any question in English or Arabic welcome to our facebook group 
+         https://www.facebook.com/groups/AToZForLearningProgramming/  
+
+
+         */
+        }
+    }
+
+}
---
+++ OperationsOn_Namespaces/OperationsOn_Namespaces/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+using YB = Brothers.YoungBrothers.MyBrothers;
+using OB = Brothers.OldBrothers.MyBrothers;
+
+namespace OperationsOn_Namespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Those are my Family ");
+            YB.print();
+            OB.print();
+            ReadKey();
+        }
+        /*  
+
+for more information visit
+         http://www.youtube.com/c/AToZForLearning 
+
+Visit our channel page on facebook
+         https://www.facebook.com/AToZForLearning/
+
+If you have any question in English or Arabic welcome to our facebook group 
+         https://www.facebook.com/groups/AToZForLearningProgramming/  
+
+
+         */
+    }
+}
