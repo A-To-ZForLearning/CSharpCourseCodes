1b1a3e9ac3b74f906e869ea18e915c69ba15690e
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-11-27 13:38:56 -0500

    Optional Paraneters

---
+++ OptionalParameters/OptionalParameters/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    class Program
+    {
+        public static void PrintValues(string name = "Nazar Al-Wattar", int myValue = 7)
+        {
+            Console.WriteLine($"Name  = {name}  Number  = {myValue}");
+
+        }
+
+        static void Main(string[] args)
+        {
+            string name = "Ahmed";
+            int value = 15;
+            PrintValues();
+            PrintValues(name);
+            PrintValues(name, value);
+            PrintValues(name: "Basim", myValue: 6);
+            Console.ReadKey();
+            /*  
+
+for more information visit
+         http://www.youtube.com/c/AToZForLearning 
+
+Visit our channel page on facebook
+         https://www.facebook.com/AToZForLearning/
+
+If you have any question in English or Arabic welcome to our facebook group 
+         https://www.facebook.com/groups/AToZForLearningProgramming/  
+
+
+         */
+
+
+        }
+    }
+}
