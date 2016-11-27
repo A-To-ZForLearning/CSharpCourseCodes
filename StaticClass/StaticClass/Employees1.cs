bc3d5915375bcd7309b9bd7ad802850d45316ae4
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-11-26 21:57:36 -0500

    Static Class

---
+++ StaticClass/StaticClass/Employees1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClass
+{
+    static class Employees1
+    {
+        static public string name = "Nazar Al-Wattar\n";
+        static public int age = 30;
+
+        static public void PrintName()
+        {
+            Console.WriteLine($"{"Name = " + name}{"AGe = " + age}");
+        }
+        /*  
+
+for more information visit
+         http://www.youtube.com/c/AToZForLearning 
+
+Visit our channel page on facebook
+         https://www.facebook.com/AToZForLearning/
+
+If you have any question in English or Arabic welcome to our facebook group 
+         https://www.facebook.com/groups/AToZForLearningProgramming/  
+
+
+         */
+
+    }
+}
---
+++ StaticClass/StaticClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Employees1.PrintName();
+            Console.ReadKey();
+            /*  
+
+for more information visit
+         http://www.youtube.com/c/AToZForLearning 
+
+Visit our channel page on facebook
+         https://www.facebook.com/AToZForLearning/
+
+If you have any question in English or Arabic welcome to our facebook group 
+         https://www.facebook.com/groups/AToZForLearningProgramming/  
+
+
+         */
+
+        }
+    }
+}
