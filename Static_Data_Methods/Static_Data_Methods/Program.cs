93955e06884f20d19b755a95912046524735a0c2
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-11-26 21:14:12 -0500

    Static Data and Methods
    In this vido we have discussed the static data and methods

---
+++ Static_Data_Methods/Static_Data_Methods/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Data_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            StaticData_and_Mehtods s1 = new StaticData_and_Mehtods(50);
+                      
+
+            // Print the current interest rate.
+            Console.WriteLine("Interest Rate is: {0}", StaticData_and_Mehtods.GetValue());
+            StaticData_and_Mehtods.SetInterestRate(0.7);
+
+            //Even If we make new object, this does NOT ’reset’ the interest rate.
+            StaticData_and_Mehtods s3 = new StaticData_and_Mehtods(2000);
+            Console.WriteLine("Interest Rate is: {0}", StaticData_and_Mehtods.GetValue());
+            Console.ReadLine();
+
+            /*  
+
+for more information visit
+         http://www.youtube.com/c/AToZForLearning 
+
+Visit our channel page on facebook
+         https://www.facebook.com/AToZForLearning/
+
+If you have any question in English or Arabic welcome to our facebook group 
+         https://www.facebook.com/groups/AToZForLearningProgramming/  
+
+
+         */
+
+        }
+    }
+}
---
+++ Static_Data_Methods/Static_Data_Methods/StaticData_and_Mehtods.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Data_Methods
+{
+    class StaticData_and_Mehtods
+    {
+        // Static Data at Instance-level.
+        public double currBalance;
+
+        // A static point of data.
+        public static double currInterestRate;
+        
+        //Parameterized Constructor
+        public StaticData_and_Mehtods(double balance)
+        {
+            currBalance = balance;
+            
+        }
+
+        static StaticData_and_Mehtods()
+        {
+            currInterestRate = 0.5;
+        }
+        public static void SetInterestRate(double newRate)
+        { currInterestRate = newRate; }
+
+        public static double GetValue()
+        { return currInterestRate; }
+
+        /*  
+
+for more information visit
+         http://www.youtube.com/c/AToZForLearning 
+
+Visit our channel page on facebook
+         https://www.facebook.com/AToZForLearning/
+
+If you have any question in English or Arabic welcome to our facebook group 
+         https://www.facebook.com/groups/AToZForLearningProgramming/  
+
+
+         */
+
+
+    }
+}
