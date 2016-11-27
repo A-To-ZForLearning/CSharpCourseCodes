ed3e6b4be6e9e15b7f709a877562d487abe1a108
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-11-27 16:25:27 -0500

    Nested Classes

---
+++ NestedClasses/NestedClasses/Employees.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedClasses
+{
+    class Employees
+    {
+        public string fName = "Ahmed Kasim";
+        public string Address = "Jefferson Rd";
+        public void PrintValues() { Console.WriteLine($"Employee Name is  {fName} \n Living at {Address}"); }
+
+        public class Departments
+        {
+            public string departmentName = "Administration";
+            public void PrintDepartmentName() { Console.WriteLine($"Working at  {departmentName}"); }
+
+        }
+    }
+}
---
+++ NestedClasses/NestedClasses/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employees myEmployee = new Employees();
+            myEmployee.PrintValues();
+
+            Employees.Departments myDepart = new Employees.Departments();
+            myDepart.PrintDepartmentName();
+
+            Console.ReadKey();
+
+            /*  
+
+for more information visit
+         http://www.youtube.com/c/AToZForLearning 
+
+Visit our channel page on facebook
+         https://www.facebook.com/AToZForLearning/
+
+If you have any question in English or Arabic welcome to our facebook group 
+         https://www.facebook.com/groups/AToZForLearningProgramming/  
+
+
+         */
+
+        }
+    }
+}
