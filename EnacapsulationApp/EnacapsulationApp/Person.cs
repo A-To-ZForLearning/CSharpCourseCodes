3cd460ef206e8fe38daa2df49f2f4ec3fb1b3ff0
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-11-27 21:46:49 -0500

    Encapsulation_In_C-Sharp

---
+++ EnacapsulationApp/EnacapsulationApp/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnacapsulationApp
+{
+    class Person
+    {
+        private string name;
+        private int age;
+        public void PrintValues()
+        { Console.WriteLine($"Name  = {name} Age = {age}"); }
+
+        //Constructors
+        public Person() { }
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        //Setters and Getters Methods
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+        public string GetName()
+        {
+            return name;
+
+        }
+        public void SetAge(int myAge)
+        {
+            age = myAge;
+        }
+        public int GetAge()
+        {
+            return age ;
+
+        }
+
+    }
+}
---
+++ EnacapsulationApp/EnacapsulationApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnacapsulationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Nazar Al-Wattar";
+            int age = 32;
+            Person myObj = new Person();
+            myObj.SetName(name);
+            myObj.SetAge(age);
+
+            Console.WriteLine("Name is " + myObj.GetName());
+            Console.WriteLine("Age is " + myObj.GetAge());
+            Console.ReadKey();
+            //Challenge 1
+            /*  
+
+for more information visit
+         http://www.youtube.com/c/AToZForLearning 
+
+Visit our channel page on facebook
+         https://www.facebook.com/AToZForLearning/
+
+If you have any question in English or Arabic welcome to our facebook group 
+         https://www.facebook.com/groups/AToZForLearningProgramming/  
+
+
+         */
+
+        }
+    }
+}
