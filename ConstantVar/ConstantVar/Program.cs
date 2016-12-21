6ba36967ee6356ec275340cdd3d3725bc84bee96
Nazar Al-Wattar <n.h.ali52014@gmail.com>
2016-12-20 22:29:11 -0500

    .
    .

---
+++ ConstantVar/ConstantVar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstantVar
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            ReadOnlyFields myObj = new ReadOnlyFields();
+
+            Console.Write($"{ReadOnlyFields.pi}");
+            Console.ReadKey();
+            /*  
+
+for more information visit
+http://www.youtube.com/c/AToZForLearning 
+
+Visit our channel page on facebook
+https://www.facebook.com/AToZForLearning/
+
+If you have any question in English or Arabic welcome to our facebook group 
+https://www.facebook.com/groups/AToZForLearningProgramming/  
+
+
+*/
+        }
+    }
+}
---
+++ ConstantVar/ConstantVar/ReadOnlyFields.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstantVar
+{
+    class ReadOnlyFields
+    {
+        public static readonly double pi;
+
+        static ReadOnlyFields()
+        {
+            pi = 3.14;
+        }
+        
+    }
+}
